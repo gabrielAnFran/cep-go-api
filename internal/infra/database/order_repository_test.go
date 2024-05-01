73bff91a93ec960cff5aa5ed8f8cce19a666092e
gabrielAnFran <antunes.f.gabriel@gmail.com>
2024-05-01 18:51:55 -0300

    feat: create order repo test

---
+++ internal/infra/database/order_repository_test.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	mockdb "cep-gin-clean-arch/mock-db"
+	"cep-gin-clean-arch/models"
+	"testing"
+)
+
+func TestBuscar(t *testing.T) {
+	repo := NewCEPRepository()
+
+	// Test case 1: CEP found
+	cep := "01001000"
+
+	expectedCEP := models.CEPResponse{Estado: "SP", Cidade: "São Paulo", Bairro: "Sé", Rua: "Praça da Sé"}
+
+	mockdb.CEPS[cep] = expectedCEP
+
+	result, err := repo.Buscar(cep)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if result != expectedCEP {
+		t.Errorf("Expected %v, got %v", expectedCEP, result)
+	}
+
+	// Test case 2: CEP not found
+	cepNotFound := "00000000"
+	_, err = repo.Buscar(cepNotFound)
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+}
