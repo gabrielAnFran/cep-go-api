29fdcd555de24bc96a09e99cdbe32d8c19088174
gabrielAnFran <antunes.f.gabriel@gmail.com>
2024-05-02 17:29:22 -0300

    refactor: melhora visualizaçao de main

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	cep_controller "cep-gin-clean-arch/controllers/cep"
 	healthcheck_controller "cep-gin-clean-arch/controllers/healthCheck"
 	token_controller "cep-gin-clean-arch/controllers/token"
+	_ "cep-gin-clean-arch/docs"
 	"cep-gin-clean-arch/internal/entity"
 	"cep-gin-clean-arch/internal/infra/database"
 	"cep-gin-clean-arch/internal/usecase"
 	middlewares "cep-gin-clean-arch/middleware"
 	"fmt"
 	"os"
-	_"cep-gin-clean-arch/docs"
 
 	"github.com/getsentry/sentry-go"
 	sentrygin "github.com/getsentry/sentry-go/gin"
@@ -38,8 +38,52 @@ import (
 func main() {
 	// Carrega variáveis de ambiente
 	godotenv.Load()
+	sentryInit()
 
-	// Inicializa o Sentry
+	// Inicializa o gin router
+	router := gin.Default()
+	router.Use(sentrygin.New(sentrygin.Options{}))
+	router.Use(cors.Default())
+	
+	// Inicializa as rotas da aplicação
+	rotas(router)
+
+	// Inicializa o servidor
+	port := fmt.Sprintf(":%s", os.Getenv("HTTP_PORT"))
+	fmt.Println("Servidor inicializado na porta:", os.Getenv("HTTP_PORT"))
+	router.Run(port)
+}
+
+func rotas(router *gin.Engine) {
+	// Rotas que necessitam de autenticação
+	api := router.Group("/")
+	api.Use(middlewares.AuthJWT())
+	{
+		// Inicializa o repositório e o handler do CEP
+		repository := entity.CEPRepositoryInterface(database.NewCEPRepository())
+		webCEPHandler := cep_controller.CEPWebHandler{CEPRepository: repository}
+		api.GET("/cep/:cep", webCEPHandler.BuscarCEP)
+		api.GET("/health-check", healthcheck_controller.HealthCheck())
+	}
+
+	// rotas que nao necessitam de autenticação
+	auth := router.Group("/")
+	{
+		// Chamada do serviço de auth para gerar um token JWT
+		jwtService := usecase.UsecaseAuth{}
+		gerarTokenHandler := token_controller.GerarTokenHandler{GerarTokenInterface: jwtService}
+		auth.POST("/gerar-token", gerarTokenHandler.GerarTokenJWT)
+	}
+
+	// Rota do swagger sem auth
+	swagger := router.Group("/docs")
+	{
+		swagger.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
+
+}
+
+func sentryInit() {
 	if err := sentry.Init(sentry.ClientOptions{
 		Dsn:              os.Getenv("SENTRY_DSN"),
 		EnableTracing:    true,
@@ -47,35 +91,4 @@ func main() {
 	}); err != nil {
 		fmt.Printf("Sentry initialization failed: %v\n", err)
 	}
-
-	// Inicializa o gin router
-	router := gin.Default()
-	router.Use(sentrygin.New(sentrygin.Options{})) // Anexa o middleware do Sentry ao gin
-	router.Use(cors.Default())                     // Viabiliza requisições de diferentes origens
-
-	// Rotas que necessitam de autenticação
-	api := router.Group("/")
-	api.Use(middlewares.AuthJWT())
-
-	// rotas que nao necessitam de autenticação
-	auth := router.Group("/")
-
-	// Inicializa o serviço de autenticação e o handler
-	jwtService := usecase.UsecaseAuth{}
-	gerarTokenHandler := token_controller.GerarTokenHandler{GerarTokenInterface: jwtService}
-	auth.POST("/gerar-token", gerarTokenHandler.GerarTokenJWT)
-
-	// Inicializa o repositório de CEP e o handler
-	repository := entity.CEPRepositoryInterface(database.NewCEPRepository())
-	webCEPHandler := cep_controller.CEPWebHandler{CEPRepository: repository}
-	api.GET("/cep/:cep", webCEPHandler.BuscarCEP)
-	api.GET("/health-check", healthcheck_controller.HealthCheck())
-
-	// Serve Swagger UI
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
-	// Inicializa o servidor
-	port := fmt.Sprintf(":%s", os.Getenv("HTTP_PORT"))
-	fmt.Println("Servidor inicializado na porta:", os.Getenv("HTTP_PORT"))
-	router.Run(port)
 }
