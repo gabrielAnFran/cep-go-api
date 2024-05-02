a578b53bc11302db162fb9ccbfe9e33bb56c2510
gabrielAnFran <antunes.f.gabriel@gmail.com>
2024-05-02 10:59:21 -0300

    feat: add model de request para o token

--- models/token.go
+++ models/token.go
@@ -1,5 +1,10 @@
 package models
 
 type TokenErrorResponse struct {
-	Error        string `json:"error" example:"Ocorreu um erro ao gerar o token"`
+	Error string `json:"error" example:"Ocorreu um erro ao gerar o token"`
+}
+
+type TokenLoginRequest struct {
+	Email string `json:"email"`
+	Senha string `json:"senha"`
 }
