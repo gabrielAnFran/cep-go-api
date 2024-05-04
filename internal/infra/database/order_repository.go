1ae60c207a7f4b5ebd1abd3a22c365ae81c4fcaf
gabrielAnFran <antunes.f.gabriel@gmail.com>
2024-05-04 12:46:09 -0300

    refactor: separa mock do file do repository e deixa num file dedicado

--- internal/infra/database/order_repository.go
+++ internal/infra/database/order_repository.go
@@ -23,44 +23,6 @@ type Address struct {
 	Rua    string `json:"Rua"`
 }
 
-const jsonDados = `
-[
-	{"CEP": "90010000", "Estado": "RS", "Cidade": "Porto Alegre", "Bairro": "Centro Histórico", "Rua": "Rua da Praia"},
-    {"CEP": "90010100", "Estado": "RS", "Cidade": "Porto Alegre", "Bairro": "Centro Histórico", "Rua": "Rua dos Andradas"},
-    {"CEP": "90020000", "Estado": "RS", "Cidade": "Porto Alegre", "Bairro": "Centro", "Rua": "Avenida Borges de Medeiros"},
-    {"CEP": "90030000", "Estado": "RS", "Cidade": "Porto Alegre", "Bairro": "Moinhos de Vento", "Rua": "Rua Mostardeiro"},
-    {"CEP": "91010000", "Estado": "RS", "Cidade": "Porto Alegre", "Bairro": "Cristo Redentor", "Rua": "Avenida Assis Brasil"},
-    {"CEP": "91020000", "Estado": "RS", "Cidade": "Porto Alegre", "Bairro": "Passo D'Areia", "Rua": "Avenida João Wallig"},
-    {"CEP": "91030000", "Estado": "RS", "Cidade": "Porto Alegre", "Bairro": "Sarandi", "Rua": "Avenida Sertório"},
-    {"CEP": "92010000", "Estado": "RS", "Cidade": "Canoas", "Bairro": "Centro", "Rua": "Rua Tiradentes"},
-    {"CEP": "92020000", "Estado": "RS", "Cidade": "Canoas", "Bairro": "Niterói", "Rua": "Avenida Santos Ferreira"},
-    {"CEP": "93010000", "Estado": "RS", "Cidade": "São Leopoldo", "Bairro": "Centro", "Rua": "Rua Independência"},
-    {"CEP": "93020000", "Estado": "RS", "Cidade": "São Leopoldo", "Bairro": "Scharlau", "Rua": "Rua São João"},
-    {"CEP": "94010000", "Estado": "RS", "Cidade": "Gravataí", "Bairro": "Centro", "Rua": "Rua Anápio Gomes"},
-    {"CEP": "94020000", "Estado": "RS", "Cidade": "Gravataí", "Bairro": "Vera Cruz", "Rua": "Avenida Dorival Cândido Luz de Oliveira"},
-    {"CEP": "95010000", "Estado": "RS", "Cidade": "Caxias do Sul", "Bairro": "Centro", "Rua": "Rua Sinimbu"},
-    {"CEP": "95020000", "Estado": "RS", "Cidade": "Caxias do Sul", "Bairro": "São Pelegrino", "Rua": "Rua Os Dezoito do Forte"},
-    {"CEP": "01001000", "Estado": "SP", "Cidade": "São Paulo", "Bairro": "Sé", "Rua": "Praça da Sé"},
-    {"CEP": "01001001", "Estado": "SP", "Cidade": "São Paulo", "Bairro": "Sé", "Rua": "Praça da Sé"},
-    {"CEP": "01153000", "Estado": "SP", "Cidade": "São Paulo", "Bairro": "Barra Funda", "Rua": "Rua Vitorino Carmilo"},
-    {"CEP": "03223050", "Estado": "SP", "Cidade": "São Paulo", "Bairro": "Jardim Independência", "Rua": "Rua Dario Meira"},
-    {"CEP": "03255000", "Estado": "SP", "Cidade": "São Paulo", "Bairro": "Vila Tolstoi", "Rua": "Rua José Antônio Fontes"},
-    {"CEP": "03513050", "Estado": "SP", "Cidade": "São Paulo", "Bairro": "Vila Matilde", "Rua": "Rua Antônio Juvenal"},
-    {"CEP": "03683010", "Estado": "SP", "Cidade": "São Paulo", "Bairro": "Vila União (Zona Leste)", "Rua": "Rua Antônio Olímpio"},
-    {"CEP": "04295001", "Estado": "SP", "Cidade": "São Paulo", "Bairro": "Vila Vera", "Rua": "Avenida Coronel José Pires de Andrade"},
-    {"CEP": "05425070", "Estado": "SP", "Cidade": "São Paulo", "Bairro": "Pinheiros", "Rua": "Avenida Doutora Ruth Cardoso"},
-    {"CEP": "05434020", "Estado": "SP", "Cidade": "São Paulo", "Bairro": "Vila Madalena", "Rua": "Rua Morás"},
-    {"CEP": "08090284", "Estado": "SP", "Cidade": "São Paulo", "Bairro": "Jardim Helena", "Rua": "Rua 03 de Outubro"},
-    {"CEP": "08410500", "Estado": "SP", "Cidade": "São Paulo", "Bairro": "Vila Santa Cruz (Zona Leste)", "Rua": "Rua Doutor Pedro Batista"},
-    {"CEP": "09925000", "Estado": "SP", "Cidade": "Diadema", "Bairro": "Campanário", "Rua": "Rodovia dos Imigrantes"},
-    {"CEP": "11441710", "Estado": "SP", "Cidade": "Guarujá", "Bairro": "Enseada", "Rua": "Rua Projetada Nove"},
-    {"CEP": "12209530", "Estado": "SP", "Cidade": "São José dos Campos", "Bairro": "Centro", "Rua": "Rua José de Alencar"},
-    {"CEP": "13327220", "Estado": "SP", "Cidade": "Salto", "Bairro": "Jardim Saltense", "Rua": "Rua Acácio Rodrigues de Moraes"},
-    {"CEP": "13401130", "Estado": "SP", "Cidade": "Piracicaba", "Bairro": "Paulista", "Rua": "Rua Sud Mennucci"},
-    {"CEP": "17022113", "Estado": "SP", "Cidade": "Bauru", "Bairro": "Vila São Paulo", "Rua": "Rua Gaudêncio Piola"},
-    {"CEP": "20020030", "Estado": "RJ", "Cidade": "Rio de Janeiro", "Bairro": "Centro", "Rua": "Praça Academia"}
-]
-`
 
 func (r *CEPRepository) Buscar(cep string) (models.CEPResponse, error) {
 
